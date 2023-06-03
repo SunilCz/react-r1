a980a674b13d4afaa3deaf5a4800993e00b7b76f
SunilCz <sunil.chaudhary7281@gmai.com>
2023-06-03 19:56:27 +0545

    Updates

---
+++ precache-manifest.961fbe539283147ac82cc05dad55b4f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6c2a9fbc4ca8277b426a",
+    "url": "/react-r1/static/css/main.8684cff0.chunk.css"
+  },
+  {
+    "revision": "6c2a9fbc4ca8277b426a",
+    "url": "/react-r1/static/js/main.29ab57e8.chunk.js"
+  },
+  {
+    "revision": "5849134a09fcec05e7eb",
+    "url": "/react-r1/static/js/runtime~main.e02f07da.js"
+  },
+  {
+    "revision": "ffdb8013ab34eaaa9343",
+    "url": "/react-r1/static/js/2.0563cc25.chunk.js"
+  },
+  {
+    "revision": "d760d958550cc5146c1d",
+    "url": "/react-r1/static/js/3.baa5238f.chunk.js"
+  },
+  {
+    "revision": "ebb8bbecc66ad4d32b6ee014ae1d98b7",
+    "url": "/react-r1/index.html"
+  }
+];
